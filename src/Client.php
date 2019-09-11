9b0d477c04f3d7f9fa34988059804f08491981c3
Nawarian <nickolas@phpsp.org.br>
2019-09-11 22:12:42 +0200

    Replace file_get_contents() implement with psr/http-client
    - Updated unit tests to be based on RequestInterface and ResponseInterface
    - Added fake implementation for http request object

--- src/Client.php
+++ src/Client.php
@@ -5,20 +5,216 @@ declare(strict_types=1);
 namespace ArchiveOrg\ItemMetadata;
 
 use ArchiveOrg\ItemMetadata\Exceptions\ItemNotFoundException;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
 
 class Client
 {
     private const METADATA_URL = 'https://archive.org/metadata/%s/metadata';
 
+    private $httpClient;
+
+    public function __construct(ClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
     public function getMetadataByIdentifier(string $identifier): Metadata
     {
-        $metadata = file_get_contents(sprintf(self::METADATA_URL, $identifier));
-        $parsedResult = json_decode($metadata, true) ?? [];
+        $response = $this->httpClient->sendRequest($this->createGetMetadataByIdentifierRequest($identifier));
 
-        if (true === array_key_exists('error', $parsedResult)) {
+        if (404 === $response->getStatusCode()) {
             throw new ItemNotFoundException("Item '{$identifier}' not found.");
         }
 
-        return new Metadata($parsedResult['result'] ?? []);
+        $parsedResult = json_decode($response->getBody()->getContents(), true)['result'] ?? [];
+
+        return new Metadata($parsedResult ?? []);
+    }
+
+    private function createGetMetadataByIdentifierRequest(string $identifier): RequestInterface
+    {
+        return new class($identifier) implements RequestInterface {
+            private $identifier;
+
+            public function __construct(string $identifier)
+            {
+                $this->identifier = $identifier;
+            }
+
+            public function getProtocolVersion()
+            {
+                // TODO: Implement getProtocolVersion() method.
+            }
+
+            public function withProtocolVersion($version)
+            {
+                // TODO: Implement withProtocolVersion() method.
+            }
+
+            public function getHeaders()
+            {
+                // TODO: Implement getHeaders() method.
+            }
+
+            public function hasHeader($name)
+            {
+                // TODO: Implement hasHeader() method.
+            }
+
+            public function getHeader($name)
+            {
+                // TODO: Implement getHeader() method.
+            }
+
+            public function getHeaderLine($name)
+            {
+                // TODO: Implement getHeaderLine() method.
+            }
+
+            public function withHeader($name, $value)
+            {
+                // TODO: Implement withHeader() method.
+            }
+
+            public function withAddedHeader($name, $value)
+            {
+                // TODO: Implement withAddedHeader() method.
+            }
+
+            public function withoutHeader($name)
+            {
+                // TODO: Implement withoutHeader() method.
+            }
+
+            public function getBody()
+            {
+                // TODO: Implement getBody() method.
+            }
+
+            public function withBody(StreamInterface $body)
+            {
+                // TODO: Implement withBody() method.
+            }
+
+            public function getRequestTarget()
+            {
+                // TODO: Implement getRequestTarget() method.
+            }
+
+            public function withRequestTarget($requestTarget)
+            {
+                // TODO: Implement withRequestTarget() method.
+            }
+
+            public function getMethod()
+            {
+                return 'GET';
+            }
+
+            public function withMethod($method)
+            {
+                // TODO: Implement withMethod() method.
+            }
+
+            public function getUri()
+            {
+                return new class($this->identifier) implements UriInterface {
+                    private $identifier;
+
+                    public function __construct(string $identifier)
+                    {
+                        $this->identifier = $identifier;
+                    }
+
+                    public function getScheme()
+                    {
+                        // TODO: Implement getScheme() method.
+                    }
+
+                    public function getAuthority()
+                    {
+                        // TODO: Implement getAuthority() method.
+                    }
+
+                    public function getUserInfo()
+                    {
+                        // TODO: Implement getUserInfo() method.
+                    }
+
+                    public function getHost()
+                    {
+                        // TODO: Implement getHost() method.
+                    }
+
+                    public function getPort()
+                    {
+                        // TODO: Implement getPort() method.
+                    }
+
+                    public function getPath()
+                    {
+                        // TODO: Implement getPath() method.
+                    }
+
+                    public function getQuery()
+                    {
+                        // TODO: Implement getQuery() method.
+                    }
+
+                    public function getFragment()
+                    {
+                        // TODO: Implement getFragment() method.
+                    }
+
+                    public function withScheme($scheme)
+                    {
+                        // TODO: Implement withScheme() method.
+                    }
+
+                    public function withUserInfo($user, $password = null)
+                    {
+                        // TODO: Implement withUserInfo() method.
+                    }
+
+                    public function withHost($host)
+                    {
+                        // TODO: Implement withHost() method.
+                    }
+
+                    public function withPort($port)
+                    {
+                        // TODO: Implement withPort() method.
+                    }
+
+                    public function withPath($path)
+                    {
+                        // TODO: Implement withPath() method.
+                    }
+
+                    public function withQuery($query)
+                    {
+                        // TODO: Implement withQuery() method.
+                    }
+
+                    public function withFragment($fragment)
+                    {
+                        // TODO: Implement withFragment() method.
+                    }
+
+                    public function __toString()
+                    {
+                        return sprintf('https://archive.org/metadata/%s/metadata', $this->identifier);
+                    }
+                };
+            }
+
+            public function withUri(UriInterface $uri, $preserveHost = false)
+            {
+                // TODO: Implement withUri() method.
+            }
+        };
     }
 }
--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -5,19 +5,38 @@ declare(strict_types=1);
 namespace ArchiveOrg\ItemMetadata;
 
 use ArchiveOrg\ItemMetadata\Exceptions\ItemNotFoundException;
+use Mockery;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class ClientTest extends TestCase
 {
     private $client;
 
+    private $fakeHttpClient;
+
     protected function setUp(): void
     {
-        $this->client = new Client();
+        $this->fakeHttpClient = Mockery::mock(ClientInterface::class);
+        $this->client = new Client($this->fakeHttpClient);
     }
 
     public function testGetMetadataByIdentifier(): void
     {
+        // phpcs:ignore Generic.Files.LineLength.TooLong
+        $jsonResponse = '{"result":{"identifier":"nawarian-test","publicdate":"2019-02-19 20:00:38","title":"nawarian-test","mediatype":"data","collection":"opensource","uploader":"nawarian@gmail.com","addeddate":"2019-02-19 20:00:38","curation":"[curator]validator@archive.org[/curator][date]20190219200101[/date][comment]checked for malware[/comment]"}}';
+
+        $this->fakeHttpClient->shouldReceive('sendRequest')
+            ->with(Mockery::on(function (RequestInterface $request) {
+                return $request->getMethod() === 'GET' &&
+                    (string) $request->getUri() === 'https://archive.org/metadata/nawarian-test/metadata';
+            }))
+            ->once()
+            ->andReturn($this->createResponseObject(200, $jsonResponse));
+
         $metadata = $this->client->getMetadataByIdentifier('nawarian-test');
 
         $this->assertEquals('nawarian-test', $metadata->identifier());
@@ -26,9 +45,39 @@ class ClientTest extends TestCase
 
     public function testGetMetadataByIdentifierWhenItemNotFound(): void
     {
+        $this->fakeHttpClient->shouldReceive('sendRequest')
+            ->with(Mockery::on(function (RequestInterface $request) {
+                $expectedUri = 'https://archive.org/metadata/hopefully-inexistent-identifier/metadata';
+
+                return $request->getMethod() === 'GET' && (string) $request->getUri() === $expectedUri;
+            }))
+            ->once()
+            ->andReturn($this->createResponseObject(404));
+
         $this->expectException(ItemNotFoundException::class);
         $this->expectExceptionMessage("Item 'hopefully-inexistent-identifier' not found.");
 
         $this->client->getMetadataByIdentifier('hopefully-inexistent-identifier');
+    }
+
+    private function createResponseObject(int $status, string $content = null): ResponseInterface
+    {
+        $fakeResponse = Mockery::mock(ResponseInterface::class);
+        $fakeResponse->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn($status);
+
+        if (null !== $content) {
+            $fakeBody = Mockery::mock(StreamInterface::class);
+            $fakeBody->shouldReceive('getContents')
+                ->once()
+                ->andReturn($content);
+
+            $fakeResponse->shouldReceive('getBody')
+                ->once()
+                ->andReturn($fakeBody);
+        }
+
+        return $fakeResponse;
     }
 }

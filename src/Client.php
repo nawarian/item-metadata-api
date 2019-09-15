5b71a4988875a9a03bf503f0b05399b6909a6fc3
Nawarian <nickolas@phpsp.org.br>
2019-09-15 23:32:37 +0200

    Rename Client::fetchHttpResponse() to Client::failIfErroredResponse()
    - Renamed Client::fetchHttpResponse() to Client::failIfErroredResponse()
    - Removed response fetching from Client::failIfErroredResponse()
    - Removed variables used only once

--- src/Client.php
+++ src/Client.php
@@ -12,7 +12,6 @@ use ArchiveOrg\ItemMetadata\Item\Identifier;
 use ArchiveOrg\ItemMetadata\Item\Item;
 use ArchiveOrg\ItemMetadata\Item\Metadata;
 use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 final class Client
@@ -29,37 +28,34 @@ final class Client
 
     public function getItemByIdentifier(Identifier $identifier): Item
     {
-        $response = $this->fetchHttpResponse($identifier, $this->requestFactory->newItemRequest($identifier));
-        $parsedResult = json_decode($response->getBody()->getContents(), true) ?? [];
+        $response = $this->httpClient->sendRequest($this->requestFactory->newItemRequest($identifier));
+        $this->failIfErroredResponse($response, $identifier);
 
-        return Item::createFromArray($parsedResult);
+        return Item::createFromArray(json_decode($response->getBody()->getContents(), true) ?? []);
     }
 
     public function getMetadataByIdentifier(Identifier $identifier): Metadata
     {
-        $response = $this->fetchHttpResponse($identifier, $this->requestFactory->newMetadataRequest($identifier));
+        $response = $this->httpClient->sendRequest($this->requestFactory->newMetadataRequest($identifier));
+        $this->failIfErroredResponse($response, $identifier);
 
-        $parsedResult = json_decode($response->getBody()->getContents(), true)['result'] ?? [];
-
-        return new Metadata($parsedResult ?? []);
+        return new Metadata(json_decode($response->getBody()->getContents(), true)['result'] ?? []);
     }
 
     public function getFilesByIdentifier(Identifier $identifier): FileCollection
     {
-        $response = $this->fetchHttpResponse($identifier, $this->requestFactory->newFilesRequest($identifier));
-
-        $parsedResult = json_decode($response->getBody()->getContents(), true)['result'] ?? [];
+        $response = $this->httpClient->sendRequest($this->requestFactory->newFilesRequest($identifier));
+        $this->failIfErroredResponse($response, $identifier);
 
         $files = array_map(function (array $file) {
             return File::createFromArray($file);
-        }, $parsedResult);
+        }, json_decode($response->getBody()->getContents(), true)['result'] ?? []);
 
         return new FileCollection($files);
     }
 
-    private function fetchHttpResponse(Identifier $identifier, RequestInterface $request): ResponseInterface
+    private function failIfErroredResponse(ResponseInterface $response, Identifier $identifier): void
     {
-        $response = $this->httpClient->sendRequest($request);
         $rawResponse = $response->getBody()->getContents();
 
         if ('{}' === $rawResponse || strpos($rawResponse, "Couldn't locate item '{$identifier}'") !== false) {
@@ -67,6 +63,5 @@ final class Client
         }
 
         $response->getBody()->rewind();
-        return $response;
     }
 }
